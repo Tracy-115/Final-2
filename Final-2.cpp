ab6aa923bd89eb77685c69d004b7644b23d83374
Tracy-115 <kmin984@insite.4cd.edu>
2025-12-11 19:11:01 -0800

    Update Final-2.cpp

--- Final-2.cpp
+++ Final-2.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 
 using namespace std;
-struct Student { //lab-11
-  string name;     
-  int id;          
-  string *request; 
-  int numRequests;
-}
-int main(){
+struct Node { //lab-11
+  string name;               
+  string drinks; 
+  Node* next;
+};
 
+int main(){
+  string names[5] = {"John", "Jane", "Joe", "Jill", "Jack"};
+  string drinks[5] = {"Coke", "Pepsi", "Sprite", "Fanta", "Dr Pepper"};
+  Node* head = nullptr;
+  Node* tail = nullptr;
+  auto back = [&] (string name, string drink) {
+    Node* newNode = new Node{name, drink, nullptr};
+    if (head == nullptr){
+      head = tail = newNode;
+    }
+    else {
+      tail -> next = newNode;
+      tail = newNode;
+    }
+  };
+  auto front = [&] () {
+    if (head == nullptr){
+      coout << "empty" << endl;
+    }
+
+    
 }
